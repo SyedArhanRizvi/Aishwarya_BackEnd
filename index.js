8914dbf323108c9eb24245a9ed3a561392365bad
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-01-24 12:08:20 +0530

    Some Changing In CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aishwarya-front-end.vercel.app",
     // credentials: true,
   })
 );
